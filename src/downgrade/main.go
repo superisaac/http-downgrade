b68465edddf9a748c2454ac2c7ff9d38d490db23
Zeng Ke <superisaac.ke@gmail.com>
2016-03-12 19:24:19 +0800

    generic handle func

--- src/downgrade/main.go
+++ src/downgrade/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/a", http.FileServer(http.Dir("/tmp/static/")))
+	//http.Handle("/a/", http.FileServer(http.Dir("/tmp/static/")))
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		fmt.Fprintf(w, "hello foo %s", q["who"])
+	})
 	http.ListenAndServe("127.0.0.1:8090", nil)
 }
