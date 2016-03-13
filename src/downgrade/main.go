8863075ee093438d3778d78795b7a650236332ba
Zeng Ke <superisaac.ke@gmail.com>
2016-03-13 20:01:11 +0800

    read post data

--- src/downgrade/main.go
+++ src/downgrade/main.go
@@ -9,7 +9,9 @@ func main() {
 	//http.Handle("/a/", http.FileServer(http.Dir("/tmp/static/")))
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 		q := r.URL.Query()
-		fmt.Fprintf(w, "hello foo %s", q["who"])
+		buf := make([]byte, 0, 10240)
+		r.Body.Read(buf)
+		fmt.Fprintf(w, "hello foo %s %s %v", q["who"], r.Method, buf)
 	})
 	http.ListenAndServe("127.0.0.1:8090", nil)
 }
