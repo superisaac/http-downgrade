ddf1d118295e3024b2e9c20e00315bc7e16647a2
Zeng Ke <superisaac.ke@gmail.com>
2016-03-28 16:21:38 +0800

    echo server

--- Package.swift
+++ Package.swift
@@ -2,5 +2,5 @@ import PackageDescription
 
 let package = Package(
         dependencies: [
-                    .Package(url: "https://github.com/CaramelForSwift/CUv.git", majorVersion: 1)
-                  ])
+                      .Package(url: "https://github.com/CaramelForSwift/CUv.git", majorVersion: 1)
+                   ])
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,47 @@
 import CUv
 
-let loop = uv_default_loop()
-uv_run(loop, UV_RUN_DEFAULT)
-print("Event loop: \(loop)")
+internal func Caramel_uv_alloc_cb(handle: UnsafeMutablePointer<uv_handle_t>, size: size_t, buf: UnsafeMutablePointer<uv_buf_t>) {
+    buf.pointee = uv_buf_init(UnsafeMutablePointer<Int8>(allocatingCapacity:size), UInt32(size))
+}
+
+internal func TCPConnection_uv_read_cb(stream: UnsafeMutablePointer<uv_stream_t>, size: Int, buf: UnsafePointer<uv_buf_t>) {
+    //let ptr = stream.pointee.data
+    //let cb = unsafeBitCast(ptr, TCPConnectionUVReadCallbackClosureBox.self).callback
+    //cb(stream, size, buf)
+    //let line = withUnsafePointer(buf.pointee.base, { (ptr) -> String? in
+    let int8Ptr = unsafeBitCast(buf.pointee.base, to: UnsafePointer<Int8>.self)
+    let line = String(validatingUTF8:int8Ptr)
+    print("read \(size) \(line)")
+}
 
 
+func on_new_connection(req: UnsafeMutablePointer<uv_stream_t>, status: Int32) {
+    let loop = uv_default_loop()
+    let client = UnsafeMutablePointer<uv_tcp_t>(allocatingCapacity:1)
+                                      defer { client.deallocateCapacity(1) }
+    var clientStream: UnsafeMutablePointer<uv_stream_t> {
+                                         return UnsafeMutablePointer<uv_stream_t>(client)
+                 }
+                                      
+    let _ = uv_tcp_init(loop, client)
+    uv_accept(req, clientStream)
+    uv_read_start(clientStream, Caramel_uv_alloc_cb, TCPConnection_uv_read_cb)
+}
+
+func main() {
+    let loop = uv_default_loop()
+    var server = UnsafeMutablePointer<uv_tcp_t>(allocatingCapacity:1)
+                                      defer { server.deallocateCapacity(1) }
+    let addr = UnsafeMutablePointer<sockaddr_in>(allocatingCapacity:1)
+                                    defer { addr.deallocateCapacity(1) }
+                                    
+    let _ = uv_ip4_addr([Int8(0),Int8(0),Int8(0),Int8(0)], Int32(9999), addr)
+    let _ = uv_tcp_init(loop, server)
+    let _ = uv_tcp_bind(server, UnsafePointer<sockaddr>(addr), 0)
+    let _ = uv_listen(UnsafeMutablePointer<uv_stream_t>(server), 1000, on_new_connection)
+    
+    uv_run(loop, UV_RUN_DEFAULT)
+    print("Event loop: \(loop)")
+}
+
+main()
