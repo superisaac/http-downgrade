dfb387c99391e662df5ec7de1f8779fe760879e5
Zeng Ke <superisaac.ke@gmail.com>
2016-03-30 17:25:47 +0800

    Write echo back

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,6 +4,15 @@ internal func Caramel_uv_alloc_cb(handle: UnsafeMutablePointer<uv_handle_t>, siz
     buf.pointee = uv_buf_init(UnsafeMutablePointer<Int8>(allocatingCapacity:size), UInt32(size))
 }
 
+internal func uv_buf_init_d(buf: UnsafeMutablePointer<Void>, _ len: UInt32) -> uv_buf_t {
+    let buffer = unsafeBitCast(buf, to: UnsafeMutablePointer<Int8>.self)
+    return uv_buf_init(buffer, len)
+}
+
+internal func TCPConnection_uv_write_cb(handle: UnsafeMutablePointer<uv_write_t>, size: Int32) {
+    print("wrote \(size)")
+}
+
 internal func TCPConnection_uv_read_cb(stream: UnsafeMutablePointer<uv_stream_t>, size: Int, buf: UnsafePointer<uv_buf_t>) {
     //let ptr = stream.pointee.data
     //let cb = unsafeBitCast(ptr, TCPConnectionUVReadCallbackClosureBox.self).callback
@@ -12,13 +21,19 @@ internal func TCPConnection_uv_read_cb(stream: UnsafeMutablePointer<uv_stream_t>
     let int8Ptr = unsafeBitCast(buf.pointee.base, to: UnsafePointer<Int8>.self)
     let line = String(validatingUTF8:int8Ptr)
     print("read \(size) \(line)")
+
+    // echo 
+    let currentWrite = UnsafeMutablePointer<uv_write_t>(allocatingCapacity:1)
+    defer { currentWrite.deallocateCapacity(1) }
+    var wbuffer = uv_buf_init_d(buf.pointee.base, UInt32(size))
+    uv_write(currentWrite, stream, &wbuffer, 1, TCPConnection_uv_write_cb)
 }
 
 
 func on_new_connection(req: UnsafeMutablePointer<uv_stream_t>, status: Int32) {
     let loop = uv_default_loop()
     let client = UnsafeMutablePointer<uv_tcp_t>(allocatingCapacity:1)
-                                      defer { client.deallocateCapacity(1) }
+    defer { client.deallocateCapacity(1) }
     var clientStream: UnsafeMutablePointer<uv_stream_t> {
                                          return UnsafeMutablePointer<uv_stream_t>(client)
                  }
