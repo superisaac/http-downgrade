d06077a80ab696da762e0afe0f51b9359231be6a
Zeng Ke <superisaac.ke@gmail.com>
2016-03-31 18:55:43 +0800

    protocol class

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,51 @@
 import CUv
 
+class Protocol {
+    var proto:Protocol? = nil
+    
+    init() {
+        print("haha")
+    }
+
+    deinit {
+        print("protocol deinit")
+    }
+    
+    func didRead(stream: UnsafeMutablePointer<uv_stream_t>, size: Int, buffer buf: UnsafePointer<uv_buf_t>) {
+        guard size >= 0 else { return }
+    let int8Ptr = unsafeBitCast(buf.pointee.base, to: UnsafeMutablePointer<Int8>.self)
+    let line = String(validatingUTF8:int8Ptr)
+    print("read \(size) \(line)")
+
+    
+    // echo 
+    let currentWrite = UnsafeMutablePointer<uv_write_t>(allocatingCapacity:1)
+    defer { currentWrite.deallocateCapacity(1) }
+    //var wbuffer = uv_buf_init_d(buf.pointee.base, UInt32(size))
+    var wbuffer = uv_buf_init(int8Ptr, UInt32(size))
+    uv_write(currentWrite, stream, &wbuffer, 1, TCPConnection_uv_write_cb)
+        
+    }
+    
+}
+
+internal typealias ReadCallback = (UnsafeMutablePointer<uv_stream_t>, Int, UnsafePointer<uv_buf_t>) -> Void
+internal typealias WriteCallback = (UnsafeMutablePointer<uv_write_t>, Int32) -> Void
+
+internal class ReadClosureBox {
+    let callback: ReadCallback
+    init(_ callback: ReadCallback) {
+        self.callback = callback
+    }
+}
+
+internal class WriteClosureBox {
+    let callback: WriteCallback
+    init(_ callback: WriteCallback) {
+        self.callback = callback
+    }
+}
+
 internal func Caramel_uv_alloc_cb(handle: UnsafeMutablePointer<uv_handle_t>, size: size_t, buf: UnsafeMutablePointer<uv_buf_t>) {
     buf.pointee = uv_buf_init(UnsafeMutablePointer<Int8>(allocatingCapacity:size), UInt32(size))
 }
@@ -14,25 +60,25 @@ internal func TCPConnection_uv_write_cb(handle: UnsafeMutablePointer<uv_write_t>
 }
 
 internal func TCPConnection_uv_read_cb(stream: UnsafeMutablePointer<uv_stream_t>, size: Int, buf: UnsafePointer<uv_buf_t>) {
-    //let ptr = stream.pointee.data
-    //let cb = unsafeBitCast(ptr, TCPConnectionUVReadCallbackClosureBox.self).callback
-    //cb(stream, size, buf)
-    //let line = withUnsafePointer(buf.pointee.base, { (ptr) -> String? in
-    let int8Ptr = unsafeBitCast(buf.pointee.base, to: UnsafeMutablePointer<Int8>.self)
-    let line = String(validatingUTF8:int8Ptr)
-    print("read \(size) \(line)")
-
-    // echo 
-    let currentWrite = UnsafeMutablePointer<uv_write_t>(allocatingCapacity:1)
-    defer { currentWrite.deallocateCapacity(1) }
-    //var wbuffer = uv_buf_init_d(buf.pointee.base, UInt32(size))
-    var wbuffer = uv_buf_init(int8Ptr, UInt32(size))
-    uv_write(currentWrite, stream, &wbuffer, 1, TCPConnection_uv_write_cb)
+    let ptr = stream.pointee.data
+    let cb = unsafeBitCast(ptr, to: ReadClosureBox.self).callback
+    cb(stream, size, buf)
+    
 }
 
 
 func on_new_connection(req: UnsafeMutablePointer<uv_stream_t>, status: Int32) {
     let loop = uv_default_loop()
+
+    var p = Protocol()
+    p.proto = p
+
+    weak var pp = p
+    var readClosure = ReadClosureBox {[weak p] handle, size, buf in
+                                      print("p is \(pp) \(p)")
+                                      pp?.didRead(handle, size: size, buffer: buf)
+    }
+    
     let client = UnsafeMutablePointer<uv_tcp_t>(allocatingCapacity:1)
     defer { client.deallocateCapacity(1) }
     var clientStream: UnsafeMutablePointer<uv_stream_t> {
@@ -42,6 +88,7 @@ func on_new_connection(req: UnsafeMutablePointer<uv_stream_t>, status: Int32) {
     let _ = uv_tcp_init(loop, client)
     uv_accept(req, clientStream)
     uv_read_start(clientStream, Caramel_uv_alloc_cb, TCPConnection_uv_read_cb)
+    client.pointee.data = unsafeBitCast(readClosure, to: UnsafeMutablePointer<Void>.self)
 }
 
 func main() {
