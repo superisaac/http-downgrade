c4e86b2766ab578c83f563e514719272b0772b58
Zeng Ke <superisaac.ke@gmail.com>
2016-04-02 22:45:32 +0800

    added HTTP Protocol

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,7 +4,7 @@ func main() {
     let server = TCPServer()
     server.serve("0.0.0.0", port:9999){() -> Protocol in
         print("get protocol")
-        return Protocol()
+        return HTTPProtocol()
     }
     server.run()
 }
